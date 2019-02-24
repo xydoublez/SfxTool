fda203b2a0b28b342b8756adc7860c5e59f82a51
ghostwinter <598850836@qq.com>
2019-02-24 17:10:43 +0800

    dd

---
+++ SfxTool/SfxFiddlerCore/SfxFiddlerCore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfxFiddlerCore
+{
+    /// <summary>
+    /// 利用fiddler类捕获请求核心类
+    /// 作者：李志强 2019/2/23 
+    /// </summary>
+    public class SfxFiddlerCore
+    {
+        /// <summary>
+        /// 开始捕获
+        /// </summary>
+        public void StartCapture()
+        {
+
+        }
+        /// <summary>
+        /// 停止捕获
+        /// </summary>
+        public void StopCapture()
+        {
+
+        }
+    }
+}
---
+++ SfxTool/SfxTool/testConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Titanium.Web.Proxy;
+using Titanium.Web.Proxy.EventArguments;
+using Titanium.Web.Proxy.Models;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Timers.Timer t = new System.Timers.Timer(1000 * 10);
+            t.Elapsed += delegate (object sender, System.Timers.ElapsedEventArgs e) { Console.WriteLine("1"); };
+            t.Start();
+            Console.ReadLine();
+
+        }
+    }
+}
